64a197bd92839aa47ba28505d9f224aaaa5b8ddc
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2022-09-28 15:14:09 +0200

    lists

---
+++ .history/pages/services_20220928151323.js
@@ -0,0 +1,11 @@
+import Meta from '../components/Meta'
+const about = () => {
+  return (
+    <div>
+    <Meta title='serv' />
+     <h1>services page</h1>
+    </div>
+  )
+}
+
+export default about
---
+++ .history/pages/services_20220928151324.js
@@ -0,0 +1,11 @@
+import Meta from '../components/Meta'
+const about = () => {
+  return (
+    <div>
+    <Meta title='service' />
+     <h1>services page</h1>
+    </div>
+  )
+}
+
+export default about
