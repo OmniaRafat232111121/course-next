f3c3e29bef1396bd9e80c74c56f16556eed5be13
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2022-09-28 15:08:27 +0200

    lists

--- .history/components/Meta_20220928150803.js
+++ .history/components/Meta_20220928150803.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Head from 'next/head'
+imp
 const Meta = ({title,keywords,description }) => {
   return (
     <Head>
--- .history/components/Meta_20220928150804.js
+++ .history/components/Meta_20220928150804.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Head from 'next/head'
+import
 const Meta = ({title,keywords,description }) => {
   return (
     <Head>
